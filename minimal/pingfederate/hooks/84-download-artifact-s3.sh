76ca976a14ba0668400dee63dafbee38899a18a0
Yasir Faruqi <yfaruqi@pingidentity.com>
2020-01-08 14:13:16 -0700

    Testing hardcoded values

--- minimal/pingfederate/hooks/84-download-artifact-s3.sh
+++ minimal/pingfederate/hooks/84-download-artifact-s3.sh
@@ -5,14 +5,17 @@ ${VERBOSE} && set -x
 # Set PATH - since this is executed from within the server process, it may not have all we need on the path
 export PATH="${PATH}:${SERVER_ROOT_DIR}/bin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:${JAVA_HOME}/bin"
 
-# Allow overriding the s3 URL with an arg
-test ! -z "${1}" && ARTIFACT_S3_URL="${1}"
+ARTIFACT_S3_URL="s3://yfaruqi-pf-artifacts-bucket"
+ARTIFACT_NAME="pf-apple-cloud-identity-connector"
+ARTIFACT_VERSION="1.0.1"
+
+#test ! -z "${1}" && ARTIFACT_S3_URL="${1}"
 echo "Downloading from location ${ARTIFACT_S3_URL}"
 
-test ! -z "${2}" && ARTIFACT_NAME="${2}"
+#test ! -z "${2}" && ARTIFACT_NAME="${2}"
 echo "Downloading Artifact ${ARTIFACT_NAME}"
 
-test ! -z "${3}" && ARTIFACT_VERSION="${3}"
+#test ! -z "${3}" && ARTIFACT_VERSION="${3}"
 echo "Downloading Artifact Version ${ARTIFACT_VERSION}"
 
 # Test command to see if the script is being executed
