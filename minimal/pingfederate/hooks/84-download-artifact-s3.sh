bd058869c2892aff0c30dace0c7b23a028b458ba
Yasir Faruqi <yfaruqi@pingidentity.com>
2020-01-09 16:43:19 -0700

    Troubleshooting

--- minimal/pingfederate/hooks/84-download-artifact-s3.sh
+++ minimal/pingfederate/hooks/84-download-artifact-s3.sh
@@ -73,7 +73,8 @@ if test ! -z "${ARTIFACT_S3_URL}"; then
 
   #ARTIFACT_DOWNLOAD_URL="s3://yfaruqi-artifact-test/IdpSample-2.8.0.war"
   #aws s3 cp "${ARTIFACT_DOWNLOAD_URL}" "${OUT_DIR}/instance/server/default/deploy"
-  aws s3 ls "${ARTIFACT_S3_URL}" > ${OUT_DIR}/list.txt
+  ALL_FILES = $( aws s3 ls ${ARTIFACT_S3_URL} )
+  echo ${ALL_FILES} > ${OUT_DIR}/list.txt
     aws s3 cp "${ARTIFACT_S3_URL}/${ARTIFACT_NAME}/${ARTIFACT_VERSION}/deploy" "${OUT_DIR}/instance/server/default/deploy" > ${OUT_DIR}/error1.txt
     #aws s3 cp "${ARTIFACT_S3_URL}/${ARTIFACT_NAME}/${ARTIFACT_VERSION}/template/" "${OUT_DIR}/instance/server/default/conf/template" --recursive > ${OUT_DIR}/error2.log
   done
