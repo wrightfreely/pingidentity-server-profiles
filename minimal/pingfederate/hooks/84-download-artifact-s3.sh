381a5358401668d627876b85f2c01bc54cd7d2b0
Yasir Faruqi <yfaruqi@pingidentity.com>
2020-01-09 12:50:49 -0700

    Troubleshooting

--- minimal/pingfederate/hooks/84-download-artifact-s3.sh
+++ minimal/pingfederate/hooks/84-download-artifact-s3.sh
@@ -54,10 +54,8 @@ if test ! -z "${ARTIFACT_S3_URL}"; then
     # Download latest artifact file from s3 bucket
     #aws s3 cp "${ARTIFACT_DOWNLOAD_URL}" "${OUT_DIR}/instance/server/default/deploy/${ARTIFACT_FILE_NAME}"
     aws s3 cp "${ARTIFACT_S3_URL}/${ARTIFACT_NAME}/${ARTIFACT_VERSION}/deploy" "${OUT_DIR}/instance/server/default/deploy" --recursive
-    aws s3 cp "${ARTIFACT_S3_URL}/${ARTIFACT_NAME}/${ARTIFACT_VERSION}/deploy" "${OUT_DIR}" --recursive
 
-    aws s3 cp "${ARTIFACT_S3_URL}/${ARTIFACT_NAME}/${ARTIFACT_VERSION}/template" "${OUT_DIR}/instance/server/default/conf/template" --recursive
-    aws s3 cp "${ARTIFACT_S3_URL}/${ARTIFACT_NAME}/${ARTIFACT_VERSION}/template" "${OUT_DIR}" --recursive
+    #aws s3 cp "${ARTIFACT_S3_URL}/${ARTIFACT_NAME}/${ARTIFACT_VERSION}/template" "${OUT_DIR}/instance/server/default/conf/template" --recursive
   done
 
   #Testing parsing json
