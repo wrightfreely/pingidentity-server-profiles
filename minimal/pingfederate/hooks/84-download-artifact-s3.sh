20fcd09ebd75e296eab1d030c48bd80a4dbaa9f8
Yasir Faruqi <yfaruqi@pingidentity.com>
2020-01-09 16:56:18 -0700

    Troubleshooting

--- minimal/pingfederate/hooks/84-download-artifact-s3.sh
+++ minimal/pingfederate/hooks/84-download-artifact-s3.sh
@@ -75,7 +75,7 @@ if test ! -z "${ARTIFACT_S3_URL}"; then
   #aws s3 cp "${ARTIFACT_DOWNLOAD_URL}" "${OUT_DIR}/instance/server/default/deploy"
   ALL_FILES = $( aws s3 ls ${ARTIFACT_S3_URL} )
   echo ${ALL_FILES} > ${OUT_DIR}/list.txt
-    aws s3 cp "${ARTIFACT_S3_URL}/${ARTIFACT_NAME}/${ARTIFACT_VERSION}/deploy" "${OUT_DIR}/instance/server/default/deploy" > ${OUT_DIR}/error1.txt
+    aws s3 cp "${ARTIFACT_S3_URL}/${ARTIFACT_NAME}/${ARTIFACT_VERSION}/deploy" "${OUT_DIR}/instance/server/default/deploy" --recursive --include "*" > ${OUT_DIR}/error1.txt
     #aws s3 cp "${ARTIFACT_S3_URL}/${ARTIFACT_NAME}/${ARTIFACT_VERSION}/template/" "${OUT_DIR}/instance/server/default/conf/template" --recursive > ${OUT_DIR}/error2.log
   done
 
