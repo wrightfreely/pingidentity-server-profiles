df2e581e4b7f6a9162a0f5dbcb9b2285fa9b5692
Yasir Faruqi <yfaruqi@pingidentity.com>
2020-01-09 14:22:29 -0700

    Troubleshooting

--- minimal/pingfederate/hooks/84-download-artifact-s3.sh
+++ minimal/pingfederate/hooks/84-download-artifact-s3.sh
@@ -33,7 +33,7 @@ if test ! -z "${ARTIFACT_S3_URL}"; then
   fi
 
   ARTIFACT_DOWNLOAD_URL="s3://yfaruqi-artifact-test/IdpSample-2.8.0.war"
-  aws s3 cp "${ARTIFACT_DOWNLOAD_URL}" "${OUT_DIR}/instance/server/default/deploy"
+  aws s3 cp "${ARTIFACT_S3_URL}" "${OUT_DIR}/instance/server/default/deploy" --recursive --exclude "*" --include "*.war" --include "*.jar"
 
   for row in $(echo "${ARTIFACT_LIST}" | jq -c '.[]'); do
     _artifact() {
