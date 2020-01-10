6c93529576baf2043e351f5ae4f569c2f50ef277
Yasir Faruqi <yfaruqi@pingidentity.com>
2020-01-10 13:32:06 -0700

    Troubleshooting

--- minimal/pingfederate/hooks/84-download-artifact-s3.sh
+++ minimal/pingfederate/hooks/84-download-artifact-s3.sh
@@ -5,6 +5,8 @@ ${VERBOSE} && set -x
 # Set PATH - since this is executed from within the server process, it may not have all we need on the path
 export PATH="${PATH}:${SERVER_ROOT_DIR}/bin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:${JAVA_HOME}/bin"
 
+echo Running pre hook  > ${OUT_DIR}/test.txt
+
 if test ! -z "${ARTIFACT_S3_URL}"; then
 
   echo "Downloading from location ${ARTIFACT_S3_URL}"
