f195ddb2108b2acc96d5f87011963dba80bbece4
Yasir Faruqi <yfaruqi@pingidentity.com>
2020-01-09 20:43:54 -0700

    Removed unused code

--- minimal/pingfederate/hooks/84-download-artifact-s3.sh
+++ minimal/pingfederate/hooks/84-download-artifact-s3.sh
@@ -7,14 +7,7 @@ export PATH="${PATH}:${SERVER_ROOT_DIR}/bin:/usr/local/bin:/usr/sbin:/usr/bin:/s
 
 if test ! -z "${ARTIFACT_S3_URL}"; then
 
-
   echo "Downloading from location ${ARTIFACT_S3_URL}"
-
-  #test ! -z "${2}" && ARTIFACT_NAME="${2}"
-  echo "Downloading Artifact ${ARTIFACT_NAME}"
-
-  #test ! -z "${3}" && ARTIFACT_VERSION="${3}"
-  echo "Downloading Artifact Version ${ARTIFACT_VERSION}"
 
   # Install AWS CLI if the upload location is S3
   if test "${ARTIFACT_S3_URL#s3}" == "${ARTIFACT_S3_URL}"; then
@@ -32,73 +25,19 @@ if test ! -z "${ARTIFACT_S3_URL}"; then
     pip3 install --no-cache-dir --upgrade jq
   fi
 
-#aws s3 cp "s3://yfaruqi-artifact-test" "${OUT_DIR}/instance/server/default/deploy" --recursive --exclude "*" --include "*.war" 2> ${OUT_DIR}/error.txt
-
-  BUCKET_URL_NO_PROTOCOL=${ARTIFACT_S3_URL#s3://}
-  BUCKET_NAME=$(echo ${BUCKET_URL_NO_PROTOCOL} | cut -d/ -f1)
-
-
   for row in $(echo "${ARTIFACT_LIST}" | jq -c '.[]'); do
     _artifact() {
       echo ${row} | jq -r ${1}
     }
-
-
     echo $(_artifact '.name')  > ${OUT_DIR}/test${row}.txt
     ARTIFACT_NAME=$(_artifact '.name')
     ARTIFACT_VERSION=$(_artifact '.version')
 
-    # Get list of files to deploy
-    #DEPLOY_FILE_LIST=$( aws s3api list-objects \
-    #  --bucket "${BUCKET_NAME}" \
-    #  --prefix "${ARTIFACT_NAME}/${ARTIFACT_VERSION}/deploy" \
-    #  --query "Contents[].{Key: Key}" --output=text )
-
-    #echo ${DEPLOY_FILE_LIST} > ${OUT_DIR}/deployfile.txt
-
-    #for deploy in $(echo "${DEPLOY_FILE_LIST}" | jq -c '.[]'); do
-    #    _deployfile() {
-    #      echo ${deploy} | jq -r ${1}
-    #    }
-
-    #    ARTIFACT_PATH=$(_deployfile '.key')
-    #    echo ${ARTIFACT_PATH} > ${OUT_DIR}/artifactpath.txt
-    #    aws s3 cp ${ARTIFACT_S3_URL}/${ARTIFACT_PATH} ${OUT_DIR}/instance/server/default/deploy
-
-    #done
-    # Test command to see if the script is being executed
-    #echo ${ARTIFACT_VERSION} > ${OUT_DIR}/test${ARTIFACT_NAME}.txt
-
-    echo "${ARTIFACT_S3_URL}/${ARTIFACT_NAME}/${ARTIFACT_VERSION}/deploy/" > ${OUT_DIR}/artifactdeploy${ARTIFACT_NAME}.txt
-
-    # Download latest artifact file from s3 bucket
-
-  #ALL_FILES=$( aws s3 ls ${ARTIFACT_S3_URL} --recursive )
-  #echo ${ALL_FILES} > ${OUT_DIR}/list.txt
-  #aws s3 cp "s3://yfaruqi-artifact-test" "${OUT_DIR}/instance/server/default/deploy" --recursive --exclude "*" --include "*.war"
     aws s3 cp "${ARTIFACT_S3_URL}/${ARTIFACT_NAME}/${ARTIFACT_VERSION}/deploy/" "${OUT_DIR}/instance/server/default/deploy" --recursive
-    #aws s3 cp "s3://yfaruqi-artifact-test/pf-apple-cloud-identity-connector/1.0.1/deploy/" "${OUT_DIR}/instance/server/default/deploy" --recursive --exclude "*" --include "*.jar" --include "*.war"
-    aws s3 cp "${ARTIFACT_S3_URL}/${ARTIFACT_NAME}/${ARTIFACT_VERSION}/template/" "${OUT_DIR}/instance/server/default/conf/template" --recursive
-
-  #ARTIFACT_DOWNLOAD_URL="s3://yfaruqi-artifact-test/IdpSample-2.8.0.war"
-  #aws s3 cp "${ARTIFACT_DOWNLOAD_URL}" "${OUT_DIR}/instance/server/default/deploy"
-
+    aws s3 cp "${ARTIFACT_S3_URL}/${ARTIFACT_NAME}/${ARTIFACT_VERSION}/conf/" "${OUT_DIR}/instance/server/default/conf" --recursive
   done
-
-  #Testing parsing json
-  echo ${ARTIFACT_LIST} > ${OUT_DIR}/artifactList.json
-  #ARTIFACT_NAME=$(jq -r .name ${OUT_DIR}/artifactList.json)
-  #ARTIFACT_VERSION=$(jq -r .version ${OUT_DIR}/artifactList.json)
-  #ARTIFACT_TYPE=$(jq -r .type ${OUT_DIR}/artifactList.json)
-  #ARTIFACT_FILE_NAME="${ARTIFACT_NAME}-${ARTIFACT_VERSION}.${ARTIFACT_TYPE}"
-  #ARTIFACT_DOWNLOAD_URL="${ARTIFACT_S3_URL}/${ARTIFACT_FILE_NAME}"
-
-  # Test command to see if the script is being executed
-  #echo ${ARTIFACT_VERSION} > ${OUT_DIR}/test${ARTIFACT_NAME}.txt
-
-  # Download latest artifact file from s3 bucket
-  #aws s3 cp "${ARTIFACT_DOWNLOAD_URL}" "${OUT_DIR}/instance/server/default/deploy/${ARTIFACT_FILE_NAME}"
 
   # Print listed files from deploy
   ls ${OUT_DIR}/instance/server/default/deploy
+  ls ${OUT_DIR}/instance/server/default/conf
 fi
