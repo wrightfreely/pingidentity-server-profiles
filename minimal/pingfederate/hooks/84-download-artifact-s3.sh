b9bbff4c6cff41cad73384f8b1230b93b4d6eea8
Yasir Faruqi <yfaruqi@pingidentity.com>
2020-01-09 18:09:35 -0700

    Troubleshooting

--- minimal/pingfederate/hooks/84-download-artifact-s3.sh
+++ minimal/pingfederate/hooks/84-download-artifact-s3.sh
@@ -67,13 +67,13 @@ if test ! -z "${ARTIFACT_S3_URL}"; then
     # Test command to see if the script is being executed
     #echo ${ARTIFACT_VERSION} > ${OUT_DIR}/test${ARTIFACT_NAME}.txt
 
-    #echo ${ARTIFACT_S3_URL}/${ARTIFACT_NAME}/${ARTIFACT_VERSION}/deploy > ${OUT_DIR}/artifactdeploy.txt
+    echo "${ARTIFACT_S3_URL}/${ARTIFACT_NAME}/${ARTIFACT_VERSION}/deploy/" > ${OUT_DIR}/artifactdeploy${ARTIFACT_NAME}.txt
 
     # Download latest artifact file from s3 bucket
 
   #ALL_FILES=$( aws s3 ls ${ARTIFACT_S3_URL} --recursive )
   #echo ${ALL_FILES} > ${OUT_DIR}/list.txt
-    aws s3 cp "${ARTIFACT_S3_URL}/${ARTIFACT_NAME}/${ARTIFACT_VERSION}/deploy/" "${OUT_DIR}/instance/server/default/deploy" --recursive --exclude "*" --include "*.jar" --include "*.war"
+    aws s3 cp "${ARTIFACT_S3_URL}/${ARTIFACT_NAME}/${ARTIFACT_VERSION}/deploy/" "/opt/out/instance/server/default/deploy" --recursive --exclude "*" --include "*.jar" --include "*.war"
     #aws s3 cp "s3://yfaruqi-artifact-test/pf-apple-cloud-identity-connector/1.0.1/deploy/" "${OUT_DIR}/instance/server/default/deploy" --recursive --exclude "*" --include "*.jar" --include "*.war"
     #aws s3 cp "${ARTIFACT_S3_URL}/${ARTIFACT_NAME}/${ARTIFACT_VERSION}/template/" "${OUT_DIR}/instance/server/default/conf/template" --recursive > ${OUT_DIR}/error2.log
 
