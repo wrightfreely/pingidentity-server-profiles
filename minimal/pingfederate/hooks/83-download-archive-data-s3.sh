0a3557322455c6faf3046e93695b6f33fd2148cf
Ray Sutton <rsutton@pingidentity.com>
2020-01-21 15:11:59 -0700

    Remove diagnostic

--- minimal/pingfederate/hooks/83-download-archive-data-s3.sh
+++ minimal/pingfederate/hooks/83-download-archive-data-s3.sh
@@ -64,7 +64,6 @@ if ! test -z "${DATA_BACKUP_FILE}"; then
   unzip "${OUT_DIR}/instance/server/default/data/drop-in-deployer/${DST_FILE}" \
       pf.jwk \
       -d "${OUT_DIR}/instance/server/default/data"
-  echo "ray ------------------>\n$(ls ${OUT_DIR}/instance/server/default/data/)\n<----------------ray"
 
   # Print the filename of the downloaded file from s3
   echo "Download file name: ${DATA_BACKUP_FILE}"
