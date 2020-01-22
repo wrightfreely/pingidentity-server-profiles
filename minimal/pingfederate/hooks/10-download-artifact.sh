8fedf7791ab68b1ce51bd028d0ab66248491e032
Yasir Faruqi <yfaruqi@pingidentity.com>
2020-01-22 07:14:30 -0700

    Updated unzip to extract only the required folders in case the zip contains other components/documents that aren't relevant

--- minimal/pingfederate/hooks/10-download-artifact.sh
+++ minimal/pingfederate/hooks/10-download-artifact.sh
@@ -86,9 +86,13 @@ if test -f "${STAGING_DIR}/artifacts/artifact-list.json"; then
             fi
 
             if test $(echo $?) == "0"; then
-              if ! unzip -o ${DOWNLOAD_DIR}/${ARTIFACT_RUNTIME_ZIP} -d ${OUT_DIR}/instance/server/default
+              if ! unzip -o ${DOWNLOAD_DIR}/${ARTIFACT_RUNTIME_ZIP} "deploy/*" -d ${OUT_DIR}/instance/server/default
               then
-                  echo Artifact ${DOWNLOAD_DIR}/${ARTIFACT_RUNTIME_ZIP} could not be unzipped.
+                  echo Artifact ${DOWNLOAD_DIR}/${ARTIFACT_RUNTIME_ZIP}/deploy could not be unzipped.
+              fi
+               if ! unzip -o ${DOWNLOAD_DIR}/${ARTIFACT_RUNTIME_ZIP} "conf/*" -d ${OUT_DIR}/instance/server/default
+              then
+                  echo Artifact ${DOWNLOAD_DIR}/${ARTIFACT_RUNTIME_ZIP}/conf could not be unzipped.
               fi
             else
               echo "Artifact download failed from ${ARTIFACT_LOCATION}"
